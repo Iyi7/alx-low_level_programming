b16d3feee7314a7aed4b9964cfea5d58cf14fd31
Iyi7 <iyisco29@gmail.com>
2022-06-08 12:58:43 -0700

    fiile

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * Print_alphabet - prints the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
